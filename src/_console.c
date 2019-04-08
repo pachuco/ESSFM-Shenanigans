360280df0f41be96aabeb8384272f89b923641e1
pachuco <ifail@life.net>
2019-04-08 21:11:25 +0300

    Now we have usable console TUI.

--- src/_console.c
+++ src/_console.c
@@ -8,49 +8,47 @@ typedef enum {
 } KSTATE;
 
 typedef struct ScreenBuffer {
-    COORD size;
+    COORD bufSize;
+    COORD wndSize;
     CHAR_INFO* data;
+    BOOL needsRedraw;
 } ScreenBuffer;
 
-static HANDLE hOut;
-static HANDLE hIn;
-static COORD originalSize;
+static HANDLE hOut = INVALID_HANDLE_VALUE;
+static HANDLE hIn = INVALID_HANDLE_VALUE;
 static DWORD whiteList;
 static BYTE keyStates[256];
 
-void getScreenSize(PCOORD pCord) {
+static BOOL trimWriteRegion(ScreenBuffer* sBuf, PSMALL_RECT rect) {
+    if (rect->Left   > sBuf->wndSize.X) return FALSE;
+    if (rect->Top    > sBuf->wndSize.Y) return FALSE;
+    if (rect->Right  > sBuf->wndSize.X) rect->Right = sBuf->wndSize.X - 1;
+    if (rect->Bottom > sBuf->wndSize.Y) rect->Right = sBuf->wndSize.Y - 1;
+    
+    return TRUE;
+}
+
+void validateScreenBuf(ScreenBuffer* sBuf) {
+    COORD wndSize, bufSize;
     CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int wipeSize;
     
-    if (!GetConsoleScreenBufferInfo(hOut, &csbi)) {
-        pCord->X = pCord->Y = 0;
-    } else {
-        pCord->X = csbi.srWindow.Right  - csbi.srWindow.Left + 1;
-        pCord->Y = csbi.srWindow.Bottom - csbi.srWindow.Top  + 1;
+    GetConsoleScreenBufferInfo(hOut, &csbi);
+    wndSize.X = csbi.srWindow.Right  - csbi.srWindow.Left + 1;
+    wndSize.Y = csbi.srWindow.Bottom - csbi.srWindow.Top  + 1;
+    if (sBuf->wndSize.X != wndSize.X || sBuf->wndSize.Y != wndSize.Y) {
+        sBuf->wndSize.X = wndSize.X;
+        sBuf->wndSize.Y = wndSize.Y;
+        sBuf->needsRedraw = TRUE;
     }
-}
-
-BOOL resizeScreenBuf(PCOORD pSize) {
-    if (!pSize) pSize = &originalSize;
-    return SetConsoleScreenBufferSize(hOut, *pSize);
-}
-
-BOOL validateScreenBuf(ScreenBuffer* buf) {
-    COORD cord;
-    
-    getScreenSize(&cord);
-    if (buf->size.X != cord.X || buf->size.Y != cord.Y) {
-        int wipeSize = cord.X * cord.Y * sizeof(CHAR_INFO);
+    if (sBuf->bufSize.X != csbi.dwSize.X || sBuf->bufSize.Y != csbi.dwSize.Y) {
+        sBuf->bufSize.X = csbi.dwSize.X;
+        sBuf->bufSize.Y = csbi.dwSize.Y;
+        sBuf->needsRedraw = TRUE;
         
-        buf->size.X = cord.X;
-        buf->size.Y = cord.Y;
-        if (buf->data) {
-            buf->data = realloc(buf->data, wipeSize);
-        } else {
-            buf->data = malloc(wipeSize);
-        }
-        return TRUE;
+        wipeSize = sBuf->bufSize.X * sBuf->bufSize.Y * sizeof(CHAR_INFO);
+        sBuf->data = !sBuf->data ? malloc(wipeSize) : realloc(sBuf->data, wipeSize);
     }
-    return FALSE;
 }
 
 void consumeEvents() {
@@ -58,10 +56,8 @@ void consumeEvents() {
     INPUT_RECORD ir;
     
     while (PeekConsoleInput(hIn, &ir, 1, &iEvNum) && iEvNum > 0) {
-        if (ir.EventType & (MENU_EVENT | FOCUS_EVENT)) {
+        if (ir.EventType & (MENU_EVENT | FOCUS_EVENT | WINDOW_BUFFER_SIZE_EVENT)) {
             //ignore these
-            ReadConsoleInput(hIn, &ir, 1, &iEvNum);
-        } else if (ir.EventType & WINDOW_BUFFER_SIZE_EVENT) {
             ReadConsoleInput(hIn, &ir, 1, &iEvNum);
         } else if (!(ir.EventType & whiteList)) {
             ReadConsoleInput(hIn, &ir, 1, &iEvNum);
@@ -76,7 +72,6 @@ KSTATE getKeyVK(WORD* out) {
     INPUT_RECORD ir;
     KSTATE ks = KEY_NONE;
     
-    consumeEvents();
     if (PeekConsoleInput(hIn, &ir, 1, &iEvNum) && iEvNum > 0 && ir.EventType == KEY_EVENT) {
         BOOL isKDown = ir.Event.KeyEvent.bKeyDown;
         WORD vk = ir.Event.KeyEvent.wVirtualKeyCode;
@@ -105,13 +100,52 @@ void clearScreen() {
    
    if (!GetConsoleScreenBufferInfo(hOut, &csbi)) return;
    dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
-   if (!FillConsoleOutputCharacter(hOut, (TCHAR) ' ', dwConSize, coordScreen, &cCharsWritten)) return;
-   if (!GetConsoleScreenBufferInfo(hOut, &csbi)) return;
+   if (!FillConsoleOutputCharacter(hOut, (CHAR) ' ', dwConSize, coordScreen, &cCharsWritten)) return;
    if (!FillConsoleOutputAttribute(hOut, csbi.wAttributes, dwConSize, coordScreen, &cCharsWritten)) return;
    SetConsoleCursorPosition(hOut, coordScreen);
 }
 
-void initTUIConsole(DWORD wl) {
+COORD paintAttributeRect(ScreenBuffer* sBuf, SMALL_RECT rect, WORD attributes) {;
+    DWORD dummy;
+    
+    if (!trimWriteRegion(sBuf, &rect)) return (COORD){-1, -1};
+    
+    for (int i=rect.Top; i < rect.Bottom; i++) {
+        for (int j=rect.Left; j < rect.Right; j++) {
+            sBuf->data[i*sBuf->bufSize.X + j].Attributes = attributes;
+        }
+    }
+    
+    SetConsoleCursorPosition(hOut, (COORD){0,0});
+    return (COORD){rect.Right, rect.Bottom};
+}
+
+COORD writeTextLine(ScreenBuffer* sBuf, COORD cord, char* format, ...) {
+    SMALL_RECT wReg;
+    char tmp[256];
+    DWORD dummy;
+    
+    va_list args;
+    va_start(args, format);
+    vsprintf(tmp,format, args);
+    va_end(args);
+    
+    wReg = (SMALL_RECT){cord.X, cord.Y, cord.X+lstrlenA(tmp), cord.Y};
+    if (!trimWriteRegion(sBuf, &wReg)) return (COORD){-1, -1};
+    
+    for (int j=wReg.Left; j < wReg.Right; j++) {
+        sBuf->data[cord.Y * sBuf->bufSize.X + j].Char.AsciiChar = tmp[j - wReg.Left];
+    }
+    
+    return (COORD){cord.Y, wReg.Right};
+}
+
+void updateRegion(ScreenBuffer* sBuf, SMALL_RECT rect) {
+    WriteConsoleOutputA(hOut, sBuf->data, sBuf->bufSize, (COORD){0,0}, &rect);
+    SetConsoleCursorPosition(hOut, (COORD){0,0});
+}
+
+void initTUIConsole(ScreenBuffer* sBuf, DWORD wl) {
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     
     whiteList = wl;
@@ -119,9 +153,6 @@ void initTUIConsole(DWORD wl) {
     hOut = GetStdHandle(STD_OUTPUT_HANDLE);
     memset(&keyStates, 0, sizeof(keyStates));
     
-    GetConsoleScreenBufferInfo(hOut, &csbi);
-    originalSize.X = csbi.dwSize.X;
-    originalSize.Y = csbi.dwSize.Y;
-    //SetConsoleMode(hIn, ENABLE_WINDOW_INPUT);
-    //SetConsoleMode(hOut, ); //ENABLE_EXTENDED_FLAGS
+    sBuf->wndSize.X = sBuf->wndSize.Y = 0;
+    sBuf->bufSize.X = sBuf->bufSize.Y = 0;
 }
--- src/main.c
+++ src/main.c
@@ -42,23 +42,25 @@ void FM_stopSynth() {
     IO_write8(0x07, 98);
 }
 
-void TUI_displayLog(Log* log, int index) {
-    COORD scrSize;
-    int vertMiddle;
-    //return;
-    getScreenSize(&scrSize);
-    vertMiddle = scrSize.Y / 2;
-    clearScreen();
-    for (int i=0; i < scrSize.Y; i++) {
+void TUI_displayLog(ScreenBuffer* sBuf, Log* log, int index) {
+    SHORT vertMiddle = sBuf->wndSize.Y / 2;
+    WORD bckgAttr  = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+    WORD pointAttr = BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE;
+    
+    paintAttributeRect(sBuf, (SMALL_RECT){0, 0, sBuf->wndSize.X, sBuf->wndSize.Y}, bckgAttr);
+    paintAttributeRect(sBuf, (SMALL_RECT){0, vertMiddle, sBuf->wndSize.X, vertMiddle+1}, pointAttr);
+    for (int i=0; i < sBuf->wndSize.Y; i++) {
         int off = index + i - vertMiddle;
         if (i != 0) printf("\n");
-        if (off >= 0 && off < log->dataSize) {
+        if (log && off >= 0 && off < log->dataSize) {
             LogRow* lr = &log->data[off];
-            printf("%07d³%2.8f³h%1X³", off, lr->duration, lr->port);
+            
+            writeTextLine(sBuf, (COORD){0, i}, "%07d\xB3%2.8f\xB3h%1X\xB3h%02X\xB3", off, lr->duration, lr->port, lr->data);
         } else {
-            printf(".......³..........³..³");
+            writeTextLine(sBuf, (COORD){0, i}, ".......\xB3..........\xB3..\xB3...\xB3");
         }
     }
+    updateRegion(sBuf, (SMALL_RECT){0, 0, sBuf->wndSize.X, sBuf->wndSize.Y});
 }
 
 int main(int argc, char *argv[]) {
@@ -69,67 +71,72 @@ int main(int argc, char *argv[]) {
         printf("Failed to init InpOut!\n");
         return 1;
     }
-    if (!loadFileToMem(&essdat, "ess.dat")) {
-        printf("Cannot load ESS data file!\n");
-        return 1;
-    }
+    //if (!loadFileToMem(&essdat, "ess.dat")) {
+    //    printf("Cannot load ESS data file!\n");
+    //    return 1;
+    //}
     
-    initTUIConsole(KEY_EVENT);
-    
-    if( argc < 2 ) { //piano mode
-        FM_startSynth();
-        pressAnyKey();
-        FM_stopSynth();
-    } else { //log player mode
+    { //log player mode
         BOOL isProgActive=TRUE, isPlaying=FALSE;
         ScreenBuffer sBuf; memset(&sBuf, 0, sizeof(ScreenBuffer));
         int index=0;
-        COORD screenSize = {128, 128};
         
-        if (!loadLog(&log, argv[1])) {
-            printf("Could not load logfile!\n");
-            return 1;
+        initTUIConsole(&sBuf, KEY_EVENT);
+        
+        if (argc >= 2) {
+            if (!loadLog(&log, argv[1])) {
+            }
         }
-        resizeScreenBuf(&screenSize);
-        
         
         while(isProgActive) {
             WORD vk; KSTATE ks;
             
-            if (validateScreenBuf(&sBuf)) {
-                TUI_displayLog(&log, index);
+            consumeEvents();
+            validateScreenBuf(&sBuf);
+            
+            if (sBuf.needsRedraw) {
+                clearScreen();
+                TUI_displayLog(&sBuf, &log, index);
+                sBuf.needsRedraw = FALSE;
             }
             
             if (isPlaying) {
             } else {
                 ks = getKeyVK(&vk);
                 if (ks & KEY_DOWN) {
+                    BOOL invLog = FALSE;
+                    
                     if (vk == VK_F1) {
                     } else if (vk == VK_PRIOR) {
-                        
+                        index -= sBuf.wndSize.Y;
+                        if (index < 0) index = 0;
+                        invLog = TRUE;
                     } else if (vk == VK_NEXT) {
-                        
+                        index += sBuf.wndSize.Y;
+                        if (index >= log.dataSize) index = log.dataSize;
+                        invLog = TRUE;
                     } else if (vk == VK_UP) {
                         if (index > 0) index--;
-                        TUI_displayLog(&log, index);
+                        invLog = TRUE;
                     } else if (vk == VK_DOWN) {
                         if (index < log.dataSize) index++;
-                        TUI_displayLog(&log, index);
+                        invLog = TRUE;
                     }
                     //non-repeating keys
                     if (ks & KEY_HEAD) { //typematic keys
                         if (vk == VK_ESCAPE) {
                             isProgActive = FALSE;
-                            clearScreen();
                         } else if (vk == VK_SPACE) {
+                            
                         }
-                    } 
+                    }
+                    if (invLog) TUI_displayLog(&sBuf, &log, index);
                 }
             }
             //if
             SleepEx(1, 1);
         }
-        resizeScreenBuf(NULL);
+        clearScreen();
     }
     
     //printf("%d\n", 0b11000);
